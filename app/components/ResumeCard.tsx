bb865e0d1d8f34adb6ecf7a367d1415437b829de
edreesz <edriselias@hotmail.com>
2025-10-30 15:22:17 -0400

    Updated root.tsx

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,22 +1,9 @@
 import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
 import {useEffect, useState} from "react";
-import {usePuterStore} from "~/lib/puter";
 
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
-    const { fs } = usePuterStore();
     const [resumeUrl, setResumeUrl] = useState('');
-
-    useEffect(() => {
-        const loadResume = async () => {
-            const blob = await fs.read(imagePath);
-            if(!blob) return;
-            let url = URL.createObjectURL(blob);
-            setResumeUrl(url);
-        }
-
-        loadResume();
-    }, [imagePath]);
 
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
